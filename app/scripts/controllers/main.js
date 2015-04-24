b4b1719bbaea4c306496d42951daa0f09f1f3413
Phil Spitler (PRS) <phil@bloc.io>
2015-04-24 12:35:38 -0400

    fixed room messages

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,11 @@
  */
 angular.module('blocChatApp')
   .controller('MainCtrl', function ($scope, $cookies, $window, $firebaseArray) {
-    var ref = new Firebase('https://yochat.firebaseio.com/rooms/');
+    var ref = new $window.Firebase('https://yochat.firebaseio.com/rooms/');
   // create an AngularFire reference to the data
   $scope.rooms = $firebaseArray(ref);
   // download the data into a local array
-  
+
 
     //USERS
     $scope.addUser = function (nickname) {
@@ -28,16 +28,9 @@ angular.module('blocChatApp')
       }
     };
 
-    //ROOMS
-    // $scope.rooms = [
-    //   {name:'Room1', messages: [{name: 'Phil', message: 'Message 1', time: '1:45 pm'}, {name: 'Patrick', message: 'Message 2', time: '2:19 pm'}]},
-    //   {name:'Room2', messages: []},
-    //   {name:'Room3', messages: []}
-    // ];
-
     $scope.addRoom = function (newRoomName) {
-      var messagesRef = ref.child('messages');
-      $scope.rooms.$add({name: newRoomName, messages: $firebaseArray(messagesRef)});
+      $scope.rooms.$add({name: newRoomName});
+
       $scope.newRoomName = '';
     };
 
@@ -45,22 +38,15 @@ angular.module('blocChatApp')
     $scope.getMessagesForRoom = function (room) {
       $scope.currentRoom = room;
       $scope.currentRoomName = room.name;
-      $scope.roomMessages = $scope.rooms.$getRecord(room.$id).messages;
-      console.log($scope.roomMessages);
-      
-      // if (roomName === 'Room1') {
-      //   $scope.roomMessages = $scope.rooms[0].messages;
-      // } else {
-      //   $scope.roomMessages = $scope.rooms[1].messages;
-      // }
+      var currentRoomMessagesRef = new $window.Firebase($scope.rooms.$ref() + '/' + room.$id + '/messages/');
+      $scope.roomMessages = $firebaseArray(currentRoomMessagesRef);
     };
 
     $scope.addMessageToRoom = function (messageText) {
-      var room = $scope.rooms;
       var nickname = $cookies.blocChatCurrentUser;
       var time = $window.moment().format('h:mm a');
-      
-      console.log($scope.rooms.$getRecord($scope.currentRoom.$id));
+
+      $scope.roomMessages.$add({name: nickname, message: messageText, time: time});
       $scope.newMessageText = '';
-    }
+    };
   });
